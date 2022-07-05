cf8dd0be111d0e869f9a8f434795df578b82de7e
henrrymbau10 <henrrymbau@gmail.com>
2022-07-05 10:29:12 +0200

    informacion desktop

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -14,6 +14,7 @@ import custom from "../../Assets/Images/Desktop/PersonalizarSVG.svg";
 export default function Contact() {
   return (
     <div class="footer-container">
+
       <div className="informacion-container">
         <div className="informacion-title">
           <h1>Información práctica</h1>
@@ -21,18 +22,12 @@ export default function Contact() {
         <div className="informacion-text">
           <div className="informacion-text-calendar">
             <img src={calendar} alt="calendario" />
-            <p>
-              <b>Fecha y hora: </b>
+            <p className="calendar-b">
+              <b >Fecha y hora: </b>
             </p>
-            <div>
-              <p>
-                Jueves: <b>17:00h y 18:00h</b>
-              </p>
-              <p>
-                Viernes: <b>17:00h, 18:00h y 19:00h</b>
-              </p>
-              <p>
-                Sábado: <b>12:00h, 17:00h y 18:00h</b>
+            <div className="p-text">
+              <p className="p">
+                Jueves: <b>17:00h y 18:00h</b> Viernes: <b>17:00h, 18:00h y 19:00h</b> Sábado: <b>12:00h, 17:00h y 18:00h</b>
               </p>
             </div>
           </div>
@@ -41,8 +36,11 @@ export default function Contact() {
           <div className="informacion-text-clock">
             <img src={clock} alt="clock" />
             <p>
-              <b>Duración: </b> 1 hora
+              <b>Duración: </b>
             </p>
+            <div className="p-text">
+              <p>1 hora</p>
+            </div>
           </div>
         </div>
         <div className="informacion-text">
@@ -51,9 +49,8 @@ export default function Contact() {
             <p>
               <b>Lugar: </b>
             </p>
-            <div>
-              <p>Reale Arena</p>
-              <p>Punto de encuentro: Puerta Hospitaly</p>
+            <div className="p-text">
+              <p>Reale Arena Punto de encuentro: Puerta Hospitaly</p>
             </div>
           </div>
         </div>
@@ -63,9 +60,8 @@ export default function Contact() {
             <p>
               <b>Menores: </b>
             </p>
-            <div>
-              <p>Acceso permitido a todos los públicos. Entrada</p>
-              <p>gratuita para menores de 5 años.</p>
+            <div className="p-text">
+              <p>Acceso permitido a todos los públicos. Entrada gratuita para menores de 5 años.</p>
             </div>
           </div>
         </div>
@@ -75,9 +71,8 @@ export default function Contact() {
             <p>
               <b>Idiomas: </b>
             </p>
-            <div>
-              <p>Euskera y Castellano | También disponible bajo</p>
-              <p> petición en ING,FRA, ALE...</p>
+            <div className="p-text">
+              <p>Euskera y Castellano | También disponible bajo petición en ING,FRA, ALE...</p>
             </div>
           </div>
         </div>
@@ -87,7 +82,7 @@ export default function Contact() {
             <p>
               <b>Cancelación: </b>
             </p>
-            <div>
+            <div className="p-text">
               <p>Cancelación gratuita hasta 7 días antes</p>
             </div>
           </div>
@@ -98,14 +93,88 @@ export default function Contact() {
             <p>
               <b>Personalizar: </b>
             </p>
-            <div>
-              <p>Para personalizar tu experiencia (cumpleaños, </p>
-              <p>empresas, grupos grandes) contactad con </p>
-              <p>hola@bstadium.es</p>
+            <div className="p-text">
+              <p>Para personalizar tu experiencia (cumpleaños, empresas, grupos grandes) contactad con hola@bstadium.es</p>
             </div>
           </div>
         </div>
       </div>
+
+      <div className="informacion-container-mobile">
+
+        <div className="informacion-container-mobile-general">
+          <div className="mobile-general-icon-calendar">
+            <img src={calendar} alt="calendario" />
+          </div>
+          <div className="mobile-general-text">
+            <p><b>Fecha y hora:</b> Jueves: <b>17:00h y 18:00h</b> Viernes: <b>17:00h, 18:00h y 19:00</b></p>
+            <p>Sábado: <b>12:00h, 17:00h y 18:00h</b></p>
+          </div>
+        </div>
+        <div className="informacion-container-mobile-general">
+          <div className="mobile-general-icon-clock">
+            <img src={clock} alt="calendario" />
+          </div>
+          <div className="mobile-general-text">
+            <p><b>Duración:</b> 1 hora</p>
+          </div>
+        </div>
+        <div className="informacion-container-mobile-general">
+          <div className="mobile-general-icon-location">
+            <img src={location} alt="calendario" />
+          </div>
+          <div className="mobile-general-text">
+            <p><b>Lugar:</b> Reale Arena</p>
+            <p><b>Punto de encuentro:</b> Puerta</p>
+            <p>Hospitally</p>
+          </div>
+        </div>
+        <div className="informacion-container-mobile-general">
+          <div className="mobile-general-icon-menor">
+            <img src={menor} alt="calendario" />
+          </div>
+          <div className="mobile-general-text">
+            <p><b>Menores:</b> Acceso permitido a </p>
+            <p>todos los públicos. Entrada </p>
+            <p>gratuita para menores de </p>
+            <p>5 años.</p>
+          </div>
+        </div>
+        <div className="informacion-container-mobile-general">
+          <div className="mobile-general-icon-language">
+            <img src={language} alt="calendario" />
+          </div>
+          <div className="mobile-general-text">
+            <p><b>Idiomas:</b> Euskera y Castellano |</p>
+            <p>También disponible bajo</p>
+            <p>petición en ING, FRA, ALE...</p>
+          </div>
+        </div>
+        <div className="informacion-container-mobile-general">
+          <div className="mobile-general-icon-declined">
+            <img src={declined} alt="calendario" />
+          </div>
+          <div className="mobile-general-text">
+            <p><b>Cancelación:</b> Cancelación </p>
+            <p>gratuita hasta 7 días antes</p>
+          </div>
+        </div>
+        <div className="informacion-container-mobile-general">
+          <div className="mobile-general-icon-custom">
+            <img src={custom} alt="calendario" />
+          </div>
+          <div className="mobile-general-text">
+            <p><b>Personalizar:</b> Para personalizar</p>
+            <p> tu experiencia (cumpleaños, </p>
+            <p>empresas, gruposgrandes) </p>
+            <p>contactad con hola@bstadium.es</p>
+          </div>
+        </div>
+
+      </div>
+
+      {/* INFORMATION CONTAINER FIN */}
+
       <div className="contacto">
         <h1>Contacto</h1>
         <p>
