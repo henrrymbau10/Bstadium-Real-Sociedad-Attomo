cc5a3cd4c97c8a414bb2e966f667c8434bea5cea
henrrymbau10 <henrrymbau@gmail.com>
2022-07-11 09:39:20 +0200

    margen

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import './App.css';
 import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
 import Main from './components/Main/Main';
-import Separator from './components/Separator/Separator';
 
 function App() {
   return (
     <div className="App">
       <Home/>
-      <Separator/>
       <Main/>
       {/* <Information/> */}
       <Contact/>
